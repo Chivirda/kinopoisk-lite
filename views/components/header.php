4d5eeafae6bdca16da87f6e5b610384c61b4c88c
Sergey Chivirda <si.chivirda@gmail.com>
2025-04-05 18:11:07 +0300

    feat: добавил модель User

--- views/components/header.php
+++ views/components/header.php
@@ -7,7 +7,7 @@ $user = $auth->user();
 ?>
 <header>
   <?php if ($auth->check()): ?>
-    <h3>User: <?= $user['email'] ?></h3>
+    <h3>User: <?= $user->email() ?></h3>
     <button>Logout</button>
     <hr>
   <?php endif; ?>
