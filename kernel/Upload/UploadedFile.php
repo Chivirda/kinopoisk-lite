346528980037e4a998dd9bc487b465ff3fc71657
Sergey Chivirda <si.chivirda@gmail.com>
2025-04-05 21:02:21 +0300

    feat: добавил класс UploadedFile

---
+++ kernel/Upload/UploadedFile.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Kernel\Upload;
+
+class UploadedFile implements UploadedFileInterface
+{
+  public function __construct(
+    public readonly string $name,
+    public readonly string $type,
+    public readonly string $tmpName,
+    public readonly int $error,
+    public readonly int $size
+  ) {}
+  public function move(string $path): bool {}
+}
--- views/pages/admin/movies/add.php
+++ views/pages/admin/movies/add.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * @var \App\Kernel\View\View $view
  * @var \App\Kernel\Session\Session $session
@@ -8,7 +9,7 @@
 <?php $view->component('start'); ?>
 <h1>Add movie page</h1>
 
-<form action="" method="post">
+<form action="" method="post" enctype="multipart/form-data">
     <label for="name">Name</label>
     <div class="form-group">
         <input type="text" name="name" id="name">
@@ -20,6 +21,18 @@
             <?php endforeach; ?>
         </ul>
     <?php endif; ?>
+    <label for="name">Image</label>
+    <div class="form-group">
+        <input type="file" name="image" id="image">
+    </div>
+    <?php if ($session->has('name')): ?>
+        <ul>
+            <?php foreach ($session->getFlash('name') as $error): ?>
+                <li><?= $error ?></li>
+            <?php endforeach; ?>
+        </ul>
+    <?php endif; ?>
+
     <div>
         <button>Add</button>
     </div>
@@ -27,6 +40,6 @@
 <?php $view->component('end'); ?>
 
 <style>
-    
+
 
 </style>
