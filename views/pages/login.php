8ab8965e72e6520714fb462b626c2f2522f580ea
Sergey Chivirda <si.chivirda@gmail.com>
2025-04-07 21:52:13 +0300

    fix: разнес компоненты в соответствии с версткой

--- views/pages/login.php
+++ views/pages/login.php
@@ -6,9 +6,9 @@
  */
 ?>
 
-<?php $view->component('start'); ?>
+<?php $view->component('start_simple'); ?>
 <main class="form-signin w-100 m-auto">
-    <form>
+    <form method="post">
         <div class="d-flex" style="align-items: center; justify-content: space-between">
             <h2 class="text-light">Вход</h2>
             <a href="/" class="d-flex align-items-center mb-5 mb-lg-0 text-white text-decoration-none">
@@ -17,12 +17,15 @@
         </div>
 
         <div class="form-floating mt-3">
-            <input type="email" class="form-control" id="floatingInput" placeholder="name@areaweb.su"> <label for="floatingInput">E-mail</label>
+            <input type="email" class="form-control" id="floatingInput" placeholder="name@areaweb.su"> <label
+                for="floatingInput">E-mail</label>
         </div>
         <div class="form-floating">
-            <input type="password" class="form-control" id="floatingPassword" placeholder="Пароль"> <label for="floatingPassword">Пароль</label>
+            <input type="password" class="form-control" id="floatingPassword" placeholder="Пароль"> <label
+                for="floatingPassword">Пароль</label>
         </div>
         <button class="btn btn-primary w-100 py-2" type="submit">Войти</button>
         <p class="mt-5 mb-3 text-light text-center">&copy; Кинопоиск Lite 2025</p>
     </form>
-</main><?php $view->component('end'); ?>
+</main>
+<?php $view->component('end_simple'); ?>
