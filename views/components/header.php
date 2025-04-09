c652f9b07262876b6f189d332fda2149182a790c
Sergey Chivirda <si.chivirda@gmail.com>
2025-04-09 23:00:28 +0300

    feat: добавил выход из аккаунта по кнопке

--- views/components/header.php
+++ views/components/header.php
@@ -54,15 +54,17 @@ $user = $auth->user();
         <?php if ($user): ?>
           <div class="d-flex align-items-center column-gap-4">
             <span class="text-white">Привет, <?= $user->name() ?>!</span>
-            <button type="button" class="btn btn-danger align-middle">
-              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none"
-                stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-                <path d="M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4" />
-                <polyline points="16 17 21 12 16 7" />
-                <line x1="21" y1="12" x2="9" y2="12" />
-              </svg>
-              <span>Выйти</span>
-            </button>
+            <form action="/logout" method="post">
+              <button type="submit" class="btn btn-danger align-middle">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none"
+                  stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                  <path d="M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4" />
+                  <polyline points="16 17 21 12 16 7" />
+                  <line x1="21" y1="12" x2="9" y2="12" />
+                </svg>
+                <span>Выйти</span>
+              </button>
+            </form>
           </div>
         <?php else: ?>
           <a href="/login" type="button" class="btn btn-outline-light me-2 d-flex align-items-center column-gap-2">
