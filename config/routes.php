97a12303b9eb7d27fdef559374f53517fe459bee
Sergey Chivirda <si.chivirda@gmail.com>
2025-04-09 23:11:54 +0300

    feat: добавил верстку админки

--- config/routes.php
+++ config/routes.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Controllers\AdminController;
 use App\Controllers\HomeController;
 use App\Controllers\LoginController;
 use App\Controllers\MovieController;
@@ -11,12 +12,13 @@ use App\Middleware\GuestMiddleware;
 return [
     Route::get('/', [HomeController::class, 'index']),
     Route::get('/movies', [MovieController::class, 'index']),
-    Route::get('/admin/movies/add', [MovieController::class, 'add'], [AuthMiddleware::class]),
-    Route::post('/admin/movies/add', [MovieController::class, 'store']),
 
     Route::get('/register', [RegisterController::class, 'index'], [GuestMiddleware::class]),
     Route::post('/register', [RegisterController::class, 'register']),
+
     Route::get('/login', [LoginController::class, 'index']),
     Route::post('/login', [LoginController::class, 'login'], [GuestMiddleware::class]),
     Route::post('/logout', [LoginController::class, 'logout']),
+
+    Route::get('/admin', [AdminController::class, 'index']),
 ];
